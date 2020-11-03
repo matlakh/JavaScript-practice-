904f8456c802453eb2b519de093ddde678b5264d
matlakh <73255302+matlakh@users.noreply.github.com>
2020-11-04 00:15:13 +0200

    Add files via upload

---
+++ lesson2_js_Matlakh/main.js
@@ -0,0 +1,15 @@
+
+let language = [
+     ['Живи','Кушай','Програмируй'],
+     ['Live','Eat','Program']
+];
+
+buttonRUS = function() {
+  for(let i = 0; i < language[0].length; i++){
+    console.log(language[0][i]);
+  }
+}
+buttonENG = function() {
+  console.log(language[1]);
+}
+
