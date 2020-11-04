461e4a5790a4e80f9268aaf6d5655d62f2c6cabb
matlakh <73255302+matlakh@users.noreply.github.com>
2020-11-04 21:32:37 +0200

    Update main.js

--- lesson2_js_Matlakh/main.js
+++ lesson2_js_Matlakh/main.js
@@ -1,15 +1,20 @@
-
 let language = [
      ['Живи','Кушай','Програмируй'],
      ['Live','Eat','Program']
 ];
 
 buttonRUS = function() {
+  console.log(language[0]);
+
   for(let i = 0; i < language[0].length; i++){
     console.log(language[0][i]);
   }
 }
 buttonENG = function() {
   console.log(language[1]);
+  
+  for(let i = 0; i < language[1].length; i++){
+    console.log(language[1][i]);
+  }
 }
 
